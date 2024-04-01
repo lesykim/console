45f2fee6caac7eaf2da713a7e61f86bc13c57b80
lesykim <trchol01@naver.com>
2024-04-01 10:32:04 +0900

    Delete updateData()

--- console/src/console/CRUD.java
+++ console/src/console/CRUD.java
@@ -3,6 +3,5 @@ package console;
 public interface CRUD {
 	public void createData(User user);
 	public void readData(User user);
-	public void updateData(User user, int number);
 	public void deleteData(User user);
 }
